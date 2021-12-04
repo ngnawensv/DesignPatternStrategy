97543df25141ddec78cc16ea21b1fc42c5955543
Ngnawen Samuel <ngnawens@gmail.com>
2021-12-04 19:10:41 +0400

    add different implementation of IStrategy Interface

---
+++ src/StrategyImpl1.java
@@ -0,0 +1,6 @@
+public class StrategyImpl1 implements IStrategy {
+    @Override
+    public void strategyApply() {
+        System.out.println("J'applique la strategy niveau 1");
+    }
+}
---
+++ src/StrategyImpl3.java
@@ -0,0 +1,7 @@
+public class StrategyImpl3 implements IStrategy {
+    @Override
+    public void strategyApply() {
+        System.out.println("J'applique la strategy niveau 3");
+
+    }
+}
