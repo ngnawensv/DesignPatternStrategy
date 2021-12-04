5938cb18ec1453d3301e0ea622b9466ed6239512
Ngnawen Samuel <ngnawens@gmail.com>
2021-12-04 19:27:48 +0400

    pattern strategy implemented

--- src/App.java
+++ src/App.java
@@ -1,7 +1,14 @@
 public class App {
     public static void main(String[] args) {
         Context context=new Context();
+        context.setStrategy(new StrategyImpl1());
+        context.strategyApply();
 
+        context.setStrategy(new StrategyImpl2());
+        context.strategyApply();
+
+        context.setStrategy(new StrategyImpl3());
+        context.strategyApply();
 
     }
 }
