f5d454912cfafa15a1fcbb342836bee8f3956de2
Ngnawen Samuel <ngnawens@gmail.com>
2021-12-04 19:18:36 +0400

    add IStrategy instance

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Context context=new Context();
-        context.strategyApply(0);
-        context.strategyApply(1);
-        context.strategyApply(2);
-        context.strategyApply(9);
+
 
     }
 }
--- src/Context.java
+++ src/Context.java
@@ -1,13 +1,10 @@
 public class Context {
-    public  void strategyApply(int level){
-        if(level == 0) {
-            System.out.println("j'applique la strategy initiale");
-        } else if (level == 1){
-            System.out.println("j'applique la strategy niveau 1");
-        }else if(level == 2){
-            System.out.println("j'applique la strategy niveau 2");
-        }else{
-            System.out.println("j'applique la strategy par defaut");
-        }
+    IStrategy strategy;
+    public  void strategyApply(){
+        strategy.strategyApply();
+
+    }
+    public void setStrategy(IStrategy strategy) {
+        this.strategy = strategy;
     }
 }
