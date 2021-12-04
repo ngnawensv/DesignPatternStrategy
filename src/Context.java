c78fd93f37185786c2920771845a424d9f60d83f
Ngnawen Samuel <ngnawens@gmail.com>
2021-12-04 18:56:37 +0400

    strategy with code open for  modification

--- src/Context.java
+++ src/Context.java
@@ -1,5 +1,13 @@
 public class Context {
-    public  void strategyApply(){
-        System.out.println("j'applique la strategy initiale");
+    public  void strategyApply(int level){
+        if(level == 0) {
+            System.out.println("j'applique la strategy initiale");
+        } else if (level == 1){
+            System.out.println("j'applique la strategy niveau 1");
+        }else if(level == 2){
+            System.out.println("j'applique la strategy niveau 2");
+        }else{
+            System.out.println("j'applique la strategy par defaut");
+        }
     }
 }
