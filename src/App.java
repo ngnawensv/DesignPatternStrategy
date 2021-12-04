6293dcf31c1ff85c879a17f410f6a1821722c332
Ngnawen Samuel <ngnawens@gmail.com>
2021-12-04 19:00:06 +0400

    strategyApply method call

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) {
         Context context=new Context();
-        context.strategyApply();
+        context.strategyApply(0);
+        context.strategyApply(1);
+        context.strategyApply(2);
+        context.strategyApply(9);
 
     }
 }
